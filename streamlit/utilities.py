1aae15a26a93ec5e80764ed64e56e641358e111f
Juan Castilla-Rho <jccastillarho@gmail.com>
2023-09-15 10:24:41 +1000

    pyvis path fixes

--- streamlit/utilities.py
+++ streamlit/utilities.py
@@ -481,13 +481,13 @@ def plot_relationships(CLD_rel_choice,CLD_isolates_choice,mode):
     
     # Save and read graph as HTML file (locally)
     except:
-        path = '/html_files'
+        path = 'html_files'
         nt.save_graph(f'{path}/pyvis_graph.html')
         HtmlFile = open(f'{path}/pyvis_graph.html','r',encoding='utf-8')
 
         # nt.show('G_factors_and_relationships.html')
         # HtmlFile = open('G_factors_and_relationships.html','r',encoding='utf-8')
-        components.html(HtmlFile.read(),height=1800)
+    components.html(HtmlFile.read(),height=1800)
         # save_graph(G)
 
     return G   

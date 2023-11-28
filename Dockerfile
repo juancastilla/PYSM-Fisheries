87af8f51b7094691eb3a4d5aad0311f2f6418a53
Juan Castilla-Rho <jccastillarho@gmail.com>
2023-11-28 12:25:22 -0500

    major improvements to Exploratory Scenario Analysis

--- Dockerfile
+++ Dockerfile
@@ -15,8 +15,11 @@ RUN apt-get update && apt-get install -y \
 #Clone repo
 RUN git clone https://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@github.com/juancastilla/PYSM-Fisheries.git .
 
-#Install re
-RUN pip3 install -r requirements.txt
+# replace RUN pip3 install -r /tmp/requirements.txt
+
+RUN python3 -m pip install --upgrade pip setuptools wheel    
+RUN python3 -m pip install numpy                                                                                                                                                                                            
+RUN python3 -m pip install -r requirements.txt  
 
 #Expose Port 8501
 EXPOSE 8501
@@ -25,4 +28,4 @@ EXPOSE 8501
 HEALTHCHECK CMD curl --fail http://localhost:8501/_stcore/health
 
 #Run Streamlit application
-ENTRYPOINT ["streamlit", "run", "Home.py", "--server.port=8501", "--server.address=0.0.0.0"]
+ENTRYPOINT ["streamlit", "run", "streamlit/Home.py", "--server.port=8501", "--server.address=0.0.0.0"]
--- streamlit/pages/03_🎯 Intervention Lab.py
+++ streamlit/pages/03_🎯 Intervention Lab.py
@@ -12,9 +12,9 @@ factors_df['TOKENS'] = 0
 
 st.title(st.session_state.fishery)
 
-with st.expander('Force-directed plot'):
+with st.expander('Influence Diagram'):
 
-    st.title('Force-directed plot')
+    st.title('Influence Diagram')
 
     relationships_filter = st.selectbox('Select relationships:', ['All relationships','Strong only'])
     domains_to_keep = st.multiselect('Select domains:', factors_df['domain_name'].unique().tolist(), default=factors_df['domain_name'].unique().tolist())
@@ -23,45 +23,132 @@ with st.expander('Force-directed plot'):
 
     G=plot_relationships_interventionlab(relationships_filter,True,'display', domains_to_remove, intervenable_filter)
 
-col1, col2 = st.columns(2)
 
-with col1:
+with st.expander('Exploratory Scenario Analysis (Single Case)'):
+    
+    st.title('Single Case')
 
-    with st.expander('Exploratory Scenario Analysis (CASE 1)'):
-        
-        st.title('Exploratory Scenario Analysis')
-
-        factors_df_display = factors_df[['factor_id',"OUTCOME NODE", "long_name", "TOKENS", "Interventions", "intervenable", "domain_name"]]
-        edited_df = st.data_editor(factors_df_display, use_container_width=False, height=1750, key='case1')
-        token_dict = edited_df[edited_df['TOKENS'] != 0].set_index('factor_id')['TOKENS'].to_dict()
+    factors_df_display = factors_df[['factor_id',"OUTCOME NODE", "long_name", "TOKENS", "Interventions", "intervenable", "domain_name"]]
+    edited_df = st.data_editor(factors_df_display, use_container_width=False, height=1750, key='case1')
+    token_dict = edited_df[edited_df['TOKENS'] != 0].set_index('factor_id')['TOKENS'].to_dict()
+    
+    col1, col2, col3, col4 = st.columns(4)
+    
+    with col1:
         log_scale = st.checkbox('Use log scale?', key='log_scale_1')
+    with col2:
         diffusion_model = st.selectbox('Choose a model:', ('one-time investment', 'continuous investment'), key='diffusion_model_1')
+    with col3:
+        time_horizon = st.slider('Time horizon (timesteps):', 1, 50, 25, key='time_horizon_1')
+    with col4: 
+        rolling_window = st.slider('Rolling window (timesteps):', 1, 5, 1, key='rolling_window_1')
 
-        if st.button('Run simulation', key='run_simulation_1'):
+    if st.button('Run simulation', key='run_simulation_1'):
 
-            if diffusion_model == 'one-time investment':
-                pulse_diffusion_network_model(G, token_dict, 40, edited_df, log_scale)
-            else:
-                flow_diffusion_network_model(G, token_dict, 40, edited_df, log_scale)
+        if diffusion_model == 'one-time investment':
+            pulse_diffusion_network_model(G, token_dict, time_horizon, edited_df, log_scale, rolling_window, vertical=False, case='case1')
+        else:
+            flow_diffusion_network_model(G, token_dict, time_horizon, edited_df, log_scale, rolling_window)
 
-with col2:
 
-    with st.expander('Exploratory Scenario Analysis (CASE 2)'):
-        
-        st.title('Exploratory Scenario Analysis')
+with st.expander('Exploratory Scenario Analysis (Compare Cases)'):
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+
+        st.title('Case 1')
 
         factors_df_display = factors_df[['factor_id',"OUTCOME NODE", "long_name", "TOKENS", "Interventions", "intervenable", "domain_name"]]
         edited_df = st.data_editor(factors_df_display, use_container_width=False, height=1750, key='case2')
         token_dict = edited_df[edited_df['TOKENS'] != 0].set_index('factor_id')['TOKENS'].to_dict()
-        log_scale = st.checkbox('Use log scale?', key='log_scale_2')
-        diffusion_model = st.selectbox('Choose a model:', ('one-time investment', 'continuous investment'), key='diffusion_model_2')
+        
+        col3, col4, col5, col6 = st.columns(4)
+        
+        with col3:
+            log_scale = st.checkbox('Use log scale?', key='log_scale_2')
+        with col4:
+            diffusion_model = st.selectbox('Choose a model:', ('one-time investment', 'continuous investment'), key='diffusion_model_2')
+        with col5:
+            time_horizon = st.slider('Time horizon (timesteps):', 1, 50, 25, key='time_horizon_2')
+        with col6: 
+            rolling_window = st.slider('Rolling window (timesteps):', 1, 5, 1, key='rolling_window_2')
 
         if st.button('Run simulation', key='run_simulation_2'):
 
             if diffusion_model == 'one-time investment':
-                pulse_diffusion_network_model(G, token_dict, 40, edited_df, log_scale)
+                pulse_diffusion_network_model(G, token_dict, time_horizon, edited_df, log_scale, rolling_window, vertical=True, case='case1')
             else:
-                flow_diffusion_network_model(G, token_dict, 40, edited_df, log_scale)
+                flow_diffusion_network_model(G, token_dict, time_horizon, edited_df, log_scale, rolling_window, vertical=True)
+
+
+    with col2:
+        
+        st.title('Case 2')
+
+        factors_df_display = factors_df[['factor_id',"OUTCOME NODE", "long_name", "TOKENS", "Interventions", "intervenable", "domain_name"]]
+        edited_df = st.data_editor(factors_df_display, use_container_width=False, height=1750, key='case3')
+        token_dict = edited_df[edited_df['TOKENS'] != 0].set_index('factor_id')['TOKENS'].to_dict()
+        
+        col7, col8, col9, col10 = st.columns(4)
+        
+        with col7:
+            log_scale = st.checkbox('Use log scale?', key='log_scale_3')
+        with col8:
+            diffusion_model = st.selectbox('Choose a model:', ('one-time investment', 'continuous investment'), key='diffusion_model_3')
+        with col9:
+            time_horizon = st.slider('Time horizon (timesteps):', 1, 50, 25, key='time_horizon_3')
+        with col10: 
+            rolling_window = st.slider('Rolling window (timesteps):', 1, 5, 1, key='rolling_window_3')
+
+        if st.button('Run simulation', key='run_simulation_3'):
+
+            if diffusion_model == 'one-time investment':
+                pulse_diffusion_network_model(G, token_dict, time_horizon, edited_df, log_scale, rolling_window, vertical=True, case='case2')
+            else:
+                flow_diffusion_network_model(G, token_dict, time_horizon, edited_df, log_scale, rolling_window, vertical=True)
+
+    if st.button('Plot Comparisons', key='plot_comparison'):
+    
+        # Get the dataframes from the session state
+        df1 = st.session_state.df_token_counts_1
+        df2 = st.session_state.df_token_counts_2
+
+        # Determine the number of rows for the subplots
+        num_rows = max(len(df1), len(df2))
+
+        # Create a new figure with subplots
+        fig, axs = plt.subplots(num_rows, 2, figsize=(20, 4*num_rows))
+
+        # Flatten the axes array
+        axs = axs.flatten()
+
+        # Get the outcome nodes
+        outcome_nodes = factors_df[factors_df['OUTCOME NODE'] == True]['long_name'].tolist()
+
+        # Iterate over each row in the DataFrame
+        for ax, (index1, row1), (index2, row2) in zip(axs, df1.iterrows(), df2.iterrows()):
+            ax.plot(row1, label='Case 1')
+            ax.plot(row2, label='Case 2')
+            # Set the title to be the row name
+            if index1 in outcome_nodes:
+                ax.set_title(index1, fontsize=20, color='red')  # Increase plot title font and set color to red for outcome nodes
+            else:
+                ax.set_title(index1, fontsize=20)  # Increase plot title font
+            ax.set_xlabel('Time step')
+            ax.set_ylabel('Token count')
+            ax.legend()  # Add legend
+
+        # Remove unused subplots
+        for ax in axs[max(len(df1), len(df2)):]:
+            fig.delaxes(ax)
+
+        # Adjust the layout
+        plt.tight_layout()
+
+        # Display the plot in Streamlit
+        st.pyplot(fig)
+
 
 with st.expander('Optimisation Analysis'):
 

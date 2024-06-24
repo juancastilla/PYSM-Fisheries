2e193c6209c5a3374a2d729073ca9b0351aff619
Juan Castilla-Rho <jccastillarho@gmail.com>
2024-06-24 10:48:00 +1000

    Intervention Lab: increase height heatmap

--- streamlit/utilities.py
+++ streamlit/utilities.py
@@ -1921,7 +1921,10 @@ def pulse_diffusion_network_model(G, initial_tokens, num_steps, df, log_scale=Fa
     if log_scale:
         df_token_counts = np.log1p(df_token_counts)
 
-    fig, ax = plt.subplots(figsize=(12, 12))
+    if st.session_state.fishery == 'Jumbo Flying Squid':
+        fig, ax = plt.subplots(figsize=(12, 18))
+    else:
+        fig, ax = plt.subplots(figsize=(12, 12))
     sns.heatmap(df_token_counts, annot=True, fmt=".1f", cmap='magma', annot_kws={"size": 7}, cbar=False, ax=ax)
 
     # Highlight outcome nodes in red
